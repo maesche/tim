5da0ac40ba231dd269603f060fa265aa63ffb144
Stefan Meier <maesche@me.com>
2011-05-19 17:08:46 +0200

    - default.TIMTest.java + default.TestSmeier.java
    Change-Id: Idef39bb6e50c8da2fc8664924354b9e302ae8a10

--- src/TestSmeier.java
+++ src/TestSmeier.java
@@ -7,7 +7,7 @@ import tim.calendar.Employee;
 import tim.application.DateHelper;
 
 
-public class TIMTest {
+public class TestSmeier {
 	public static void main(String[] args) {
 		
 		AppointmentHandler appHandler = new AppointmentHandler();
