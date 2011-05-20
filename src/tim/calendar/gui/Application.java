b25b9572e24a1683e68e1e7c3e2b098ae83a6dfc
Stefan Meier <maesche@me.com>
2011-05-20 17:21:28 +0200

    + tim.calendar.gui.Application
    Change-Id: I53cf85121aa160d8ea0f71b607aaf9ba35df4e1a

---
+++ src/tim/calendar/gui/Application.java
@@ -0,0 +1,7 @@
+package tim.calendar.gui;
+
+import javax.swing.JFrame;
+
+public class Application extends JFrame {
+
+}
--- src/tim/calendar/model/Employee.java
+++ src/tim/calendar/model/Employee.java
@@ -5,14 +5,9 @@ public class Employee extends Person {
 	public Employee(int id, String firstName, String lastName, String phone,
 			String address, String comment) {
 		super(id, firstName, lastName, phone, address, comment);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public Employee(int id, String firstName, String lastName) {
 		super(id, firstName, lastName);
-	}
-
-	public String test() {
-		return "test";
 	}
 }
