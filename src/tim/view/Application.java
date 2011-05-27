8923994d162efa5ed83635d6eeebd2af3f74c8f4
Stefan Meier <maesche@me.com>
2011-05-27 17:26:56 +0200

    + tim.view.EventDialog
    Change-Id: I78501e24273d15f8aaa18602aae7d11c1133f6d7

--- src/tim/view/Application.java
+++ src/tim/view/Application.java
@@ -2,16 +2,23 @@ package tim.view;
 
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
 import tim.application.ErrorHandler;
 
 public class Application extends JFrame {
+	
+	JButton btnDialog;
+	JDialog eventDialog;
+	
 	public Application() {
 		try {
 			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
@@ -26,9 +33,18 @@ public class Application extends JFrame {
 				System.exit(0);
 			}
 		});
-
+		btnDialog = new JButton("Dialogue");
 		Container container = getContentPane();
 
-		container.add(new JButton("test"));
+		btnDialog.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				eventDialog.setLocationRelativeTo(Application.this);
+				eventDialog.setVisible(true);
+			}
+			
+		});
+		
+		container.add(btnDialog);
 	}
 }
---
+++ src/tim/view/EventDialog.java
@@ -0,0 +1,7 @@
+package tim.view;
+
+import javax.swing.JDialog;
+
+public class EventDialog extends JDialog {
+
+}
