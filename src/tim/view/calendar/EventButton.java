a3a288301cd9a5276b289f63a5ee6add3806c1c4
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2011-06-09 16:52:26 +0200

    @ tim.view.calendar.EventButton

--- src/tim/view/calendar/EventButton.java
+++ src/tim/view/calendar/EventButton.java
@@ -10,11 +10,14 @@ import tim.model.Appointment;
 
 public class EventButton extends JButton {
 		private String title;
+		private long duration;
 
 	public EventButton(Appointment a) {
 		
-		this.setText(toString(a));
+		this.title = toString(a);
+		this.setText(this.title);
 		this.setMargin(new Insets(0, 2, 0, 2));
+		this.duration = a.getBegin().getTime();//(a.getEnd().getTime() - a.getBegin().getTime());
 	}
 	
 	
@@ -27,14 +30,13 @@ public class EventButton extends JButton {
 		String title;
 		title = "<html>";
 		//title += a.getEmployee().getFirstName() + "<br />";
-		title += DateHelper.DateToString(a.getBegin(), Config.DATE_FORMAT_EVENT_HOUR) + "<br />";
+		title += DateHelper.DateToString(a.getBegin(), Config.DATE_FORMAT_EVENT_HOUR) + " - " + DateHelper.DateToString(a.getEnd(), Config.DATE_FORMAT_EVENT_HOUR) + "<br />";
 		title += a.getTitle() + "<br />";
 		title += "with " + a.getClient().getFirstName() + " " + a.getClient().getLastName() + "<br />";
 		title += a.getDescription() + "<br />";
+		title += "durée: " + String.valueOf(this.duration) + "<br />";
 		
 		title += "</html>";
-		
-		System.out.println(a.toString());
 		
 		return title;
 		
