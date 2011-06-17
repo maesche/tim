0f15ba78fb1486a4ad63d535a0a0eac5f1eea903
Stefan Meier <maesche@me.com>
2011-06-17 15:53:08 +0200

    @ TIM (Font change to support japanese text)
    Change-Id: Ic8b6477092af16d02a0935f16ad5fa785a364792

--- src/TIM.java
+++ src/TIM.java
@@ -1,8 +1,11 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.util.Enumeration;
 
 import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.FontUIResource;
 
 import tim.view.Application;
 
@@ -12,22 +15,26 @@ import tim.controller.ApplicationController;
 
 public class TIM {
 	
-	private static void setUIFont(javax.swing.plaf.FontUIResource f)
+	private static void setUIFont(FontUIResource f)
 	{
 	    Enumeration<Object> keys = UIManager.getDefaults().keys();
 	    while (keys.hasMoreElements())
 	    {
 	        Object key = keys.nextElement();
 	        Object value = UIManager.get(key);
-	        if (value instanceof javax.swing.plaf.FontUIResource)
+	        if (value instanceof FontUIResource)
 	        {
 	            UIManager.put(key, f);
+	        }
+	        
+	        if (value instanceof ColorUIResource && !((String) key).toLowerCase().endsWith("background")) {
+	        	//UIManager.put(key, new ColorUIResource(Color.black));
 	        }
 	    }
 	}
 	
 	public static void main(String[] args) {
-		setUIFont (new javax.swing.plaf.FontUIResource(new Font("Arial Unicode MS", Font.BOLD, 14)));
+		setUIFont (new FontUIResource(new Font("Arial Unicode MS", Font.BOLD, 14)));
 		ApplicationController applicationController = new ApplicationController();
 		applicationController.init();
 		
