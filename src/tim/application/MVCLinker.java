727db8755196c11da4ae65201bc543327956aa5c
Stefan Meier <maesche@me.com>
2011-06-18 21:18:32 +0200

    @ tim.application.MVCLinker (add controller registry)
    Change-Id: Iedbee45931a33c087237850c5ce2297919a8dc17

--- src/tim/application/MVCLinker.java
+++ src/tim/application/MVCLinker.java
@@ -2,41 +2,67 @@ package tim.application;
 
 import java.util.HashMap;
 
+import tim.controller.AbstractController;
 import tim.model.AbstractModel;
 import tim.view.AbstractView;
 
 public class MVCLinker {
-	private HashMap <String, AbstractModel> models;
+	private HashMap<String, AbstractModel> models;
+	private HashMap<String, AbstractController> controllers;
 	
 	public MVCLinker() {
-		models = new HashMap <String, AbstractModel>();
+		models = new HashMap<String, AbstractModel>();
+		controllers = new HashMap<String, AbstractController>();
 	}
 	
-	
+	/**
+	 * Get all globally registered models
+	 * @return models
+	 */
 	public HashMap<String, AbstractModel> getModels() {
 		return models;
 	}
 
-	public void addModel(AbstractModel model) {
+	/**
+	 * Register model to global register
+	 * 
+	 * @param model
+	 */
+	public void registerModel(AbstractModel model) {
 		models.put(model.toString(), model);
-		try {
-			Class.forName("tim.model." + "AppointmentModel");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
 	}
 	
-	public void removeModel(AbstractModel model) {
+	/**
+	 * Unregister model from global register
+	 * @param model
+	 */
+	public void unregisterModel(AbstractModel model) {
 		models.remove(model.toString());
 	}
 
 
-
-	public void addToObserverRegistry(AbstractModel model, AbstractView view) {
+	/**
+	 * Get all globally registered controllers
+	 * 
+	 * @return controllers
+	 */
+	public HashMap<String, AbstractController> getControllers() {
+		return controllers;
+	}
+	
+	public void registerController(AbstractController controller) {
+		controllers.put(controller.toString(), controller);
+	}
+	
+	public void unregisterController(AbstractController controller) {
+		controllers.remove(controller.toString());
+	}
+	
+	public void addObserverToModel(AbstractModel model, AbstractView view) {
 		model.addObserver(view);
 	}
 	
-	public void removeFromObserverRegistry(AbstractModel model, AbstractView view) {
+	public void removeObserverFromModel(AbstractModel model, AbstractView view) {
 		model.deleteObserver(view);
 	}
 }
