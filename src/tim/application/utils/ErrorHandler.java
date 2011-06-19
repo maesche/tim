10fa90a00e6cd240da250c255b14a6efd4d2c3b6
Stefan Meier <maesche@me.com>
2011-06-19 21:21:57 +0200

    @ tim.application.utils.ErrorHandler (deprecated)
    Change-Id: I93a363d9e23b0d130b2669c76193268976392406

--- src/tim/application/utils/ErrorHandler.java
+++ src/tim/application/utils/ErrorHandler.java
@@ -2,7 +2,13 @@ package tim.application.utils;
 
 public class ErrorHandler {
 	
-	/*public static void getException(Exception ex, String className, String methodName) {
+	/**
+	 * @deprecated please use tim.application.exception and forward Exceptions to view
+	 * @param ex
+	 * @param className
+	 * @param methodName
+	 */
+	public static void getException(Exception ex, String className, String methodName) {
 		String error = "--------------------------\n";
 		error += "Class:\t\t" + className + "\n";
 		error += "Method:\t\t" + methodName + "\n";
@@ -11,5 +17,5 @@ public class ErrorHandler {
 		error += "--------------------------\n";
 
 		System.err.println(error);
-	}*/
+	}
 }
